b53b22f7b72350d9fc688d3a9799c98b1f820e23
burakcanozdag <burakcanozdag9@mail.com>
2025-05-09 15:48:36 +0300

    giris son hali(php) ana sayfa baslangıc

---
+++ giris.php
@@ -0,0 +1,22 @@
+<?php
+
+$dogru_email="b241210097@gmail.com";
+$dogru_sifre="b241210097";
+
+$email=$_POST['email'];
+$sifre=$_POST['sifre'];
+
+
+if($dogru_email==$email && $dogru_sifre==$sifre){
+    header("Refresh:3;url=anasayfa.html");
+    echo "Hoşgeldin b241210097! Ana sayfaya yönlendiriliyorsun.";
+    exit();
+}
+else{
+    header("Refresh:3;url=giris.html");
+    echo "Girdiğiniz email veya şifre hatalı!";
+}
+
+?>
+
+
